55cbc678d37cc771cc3faf358c036330d032bb62
Mirza Irwan Osman <suratjebat@gmail.com>
2017-01-06 02:45:52 +0800

    Add navigation drawer.

---
+++ app/src/main/java/com/mirzairwan/shopping/NavItem.java
@@ -0,0 +1,16 @@
+package com.mirzairwan.shopping;
+
+/**
+ * Created by Mirza Irwan on 6/1/17.
+ */
+public class NavItem
+{
+    public int text;
+    public int drawable;
+
+    public NavItem(int text, int drawable)
+    {
+        this.text = text;
+        this.drawable = drawable;
+    }
+}
--- app/src/main/java/com/mirzairwan/shopping/ShoppingActivity.java
+++ app/src/main/java/com/mirzairwan/shopping/ShoppingActivity.java
@@ -2,13 +2,20 @@ package com.mirzairwan.shopping;
 
 import android.content.ContentUris;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.mirzairwan.shopping.data.AndroidDatabaseManager;
 import com.mirzairwan.shopping.data.Contract;
@@ -18,11 +25,14 @@ import static com.mirzairwan.shopping.ItemEditingActivity.ITEM_IS_IN_SHOPPING_LI
 import static com.mirzairwan.shopping.R.id.menu_database_shopping_list;
 
 public class ShoppingActivity extends AppCompatActivity implements
-                        ShoppingListFragment.OnFragmentInteractionListener,
-                        CatalogFragment.OnFragmentInteractionListener
+        ShoppingListFragment.OnFragmentInteractionListener,
+        CatalogFragment.OnFragmentInteractionListener
 {
     public static final String HOME_COUNTRY_CODE = "HOME_COUNTRY_CODE";
     public static final String PERSONAL = "PERSONAL";
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,9 +43,54 @@ public class ShoppingActivity extends AppCompatActivity implements
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
         PagerAdapter pagerAdapter = new PagerAdapter(getFragmentManager(), this);
-        ViewPager viewPager = (ViewPager)findViewById(R.id.pager_shopping);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager_shopping);
         viewPager.setAdapter(pagerAdapter);
+
+        setUpNavDrawer();
     }
+
+    private void setUpNavDrawer()
+    {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        NavDrawerArrayAdapter navDrawerAdapter = new NavDrawerArrayAdapter
+                (this, R.layout.nav_drawer_row, new NavItem[]
+                        {new NavItem(R.string.settings_screen, R.drawable.ic_settings)}
+                );
+
+        mDrawerList.setAdapter(navDrawerAdapter);
+
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout, R.string.drawer_open,
+                R.string.drawer_close);
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP
+                | ActionBar.DISPLAY_SHOW_TITLE);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
@@ -48,8 +103,8 @@ public class ShoppingActivity extends AppCompatActivity implements
     public boolean onOptionsItemSelected(MenuItem menuItem)
     {
         int menuItemId = menuItem.getItemId();
-        switch(menuItemId)
-        {
+
+        switch (menuItemId) {
             case menu_database_shopping_list:
                 Intent intentDb = new Intent(this, AndroidDatabaseManager.class);
                 startActivity(intentDb);
@@ -59,7 +114,12 @@ public class ShoppingActivity extends AppCompatActivity implements
                 startActivity(intentSettings);
                 return true;
             default:
-                return super.onOptionsItemSelected(menuItem);
+                // Pass the event to ActionBarDrawerToggle, if it returns
+                // true, then it has handled the app icon touch event
+                if (mDrawerToggle.onOptionsItemSelected(menuItem)) {
+                    return true;
+                } else
+                    return super.onOptionsItemSelected(menuItem);
         }
     }
 
@@ -98,6 +158,17 @@ public class ShoppingActivity extends AppCompatActivity implements
         Uri uri = ContentUris.withAppendedId(ItemsEntry.CONTENT_URI, itemId);
         intentToViewItem.setData(uri);
         startActivity(intentToViewItem);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener
+    {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        {
+            mDrawerLayout.closeDrawer(mDrawerList);
+            Intent intentSettings = new Intent(ShoppingActivity.this, SettingsActivity.class);
+            startActivity(intentSettings);
+        }
     }
 
 
